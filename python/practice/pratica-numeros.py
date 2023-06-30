952831b72a280e6d40522ac2544c630ff95881a3
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-06-30 13:46:47 -0300

    Uploading the file to GitHub

---
+++ python/practice/pratica-numeros.py
@@ -0,0 +1,30 @@
+print("Bem-vindo a minha primeira calculadora básica de soma, subtração, multiplicação e divisão!")
+print("OBS: Como ainda não estudei operadores lógicos e de comparação em Python, então eu irei esta de forma bem simples e mais \"enxuta\" ")
+
+print("\nSOMA")
+
+num1 = float(input("\nDigite um número de sua escolha: "))
+num2 = float(input("Digite um segundo número de sua escolha: "))
+
+print(f"\nO resultado da sua conta de soma é {num1 + num2}")
+
+print("\nSUBTRAÇÃO")
+
+num1 = float(input("\nDigite um número de sua escolha: "))
+num2 = float(input("Digite um segundo número de sua escolha: "))
+
+print(f"\nO resultado da sua conta de soma é {num1 - num2}")
+
+print("\nMULTIPLICAÇÃO")
+
+num1 = float(input("\nDigite um número de sua escolha: "))
+num2 = float(input("Digite um segundo número de sua escolha: "))
+
+print(f"\nO resultado da sua conta de soma é {num1 * num2}")
+
+print("\nDIVISÃO")
+
+num1 = float(input("\nDigite um número de sua escolha: "))
+num2 = float(input("Digite um segundo número de sua escolha: "))
+
+print(f"\nO resultado da sua conta de soma é {num1 / num2}")
