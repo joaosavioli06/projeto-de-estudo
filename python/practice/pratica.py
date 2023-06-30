85e9a458f99eb7e1836df66f109dc9e1ccccd547
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-06-30 12:47:38 -0300

    Changing the file's place

---
+++ python/practice/pratica.py
@@ -0,0 +1,23 @@
+nome = ("João Gabriel")
+idade = 17
+linguagem = ("Python")
+objetivo = ("Programador")
+ano = 2023
+
+
+print(f"Olá! Meu nome é {nome} e tenho {idade} anos. Este código é apenas para fazer um simples teste, até porque estou no ínicio dos meus estudos da linguagem {linguagem}.")
+print(f"Tenho o alvo de ser um {objetivo} no futuro, mas sei que devo estudar muito para conseguir alcançar este tão sonhado objetivo.")
+print(f"O ano de {ano} está sendo incrível, estou focando bastante no meu aprendizado individual e estou colhendo vários resultados")
+
+print("\nNo entanto, agora quero saber um pouco mais sobre você. Primeiramente, me diga o seu nome")
+nome = input("Digite o seu nome: ")
+
+print(f"\nMuito bem, prazer em te conhecer {nome}! ")
+
+print(f"\nOk {nome}, agora me fale sua idade, quero saber em qual nível de experiência você está!")
+
+idade = int(input("Digite sua idade: "))
+
+print(f"\nUau, você tem {idade} anos de idade")
+
+print(f"\nPrazer em conhecê-lo {nome}. Você é o meu primeiro contato com o input dentro do Python :)")
