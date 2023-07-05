2ea5f9d393d8f977f9acc76a6848b80a56690c59
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-07-05 17:01:59 -0300

    Adding new fle

---
+++ python/course-notes/if-else.py
@@ -0,0 +1,70 @@
+#tenho_sede = False
+
+#if tenho_sede:
+#print("Beber água")
+
+#print("Vida que segue")
+
+#frio = False
+
+#if frio:
+    #print("Casaco")
+#else:
+    #print("Camiseta")
+
+sede = True
+fome = False
+
+cafeina = True
+
+#if sede or fome:
+#    print("Ir para cozinha")
+#else:
+#    print("Não ir para cozinha")
+
+
+#if sede and fome:
+#    print("Ir para cozinha")
+#else:
+#    print("Não ir para cozinha")
+#se uma das condições for false, ele vai cair na situação de baixo. Se ambos forem verdadeiros, ele vai printar a primeira situação
+
+#if sede and fome:
+#    print("Ir para cozinha")
+#elif sede and not (fome):
+#    if cafeina:
+#        print("Beber café")
+#    else:
+#        print("Beber água")
+#elif not (sede) and fome:
+#   print("Ir preparar algo para comer")
+#else:
+#   print("Relax")
+
+num1 = 32
+num2 = 32
+
+if num1 <= num2:
+    print("num1 é menor ou igual num2")
+#elif num1 != num2:
+#    print("não são iguais")
+#elif num1 > num2:
+#    print("num1 é maior que num2")
+#elif num1 < num2:
+#    print("num1 é menor que num2")
+
+
+#operadores de comparação
+# == -> igual
+# != -> diferente
+# >  -> maior
+# <  -> menor
+# >= -> maior ou igual
+# <= -> menor ou igual
+
+#operadores lógicos
+# or -> ou
+# and -> e
+# not () = negação
+
+
