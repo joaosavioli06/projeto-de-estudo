fc98becac7a7e58bb2fe955746e78fe5079cd1bf
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-07-19 17:09:58 -0300

    Updating file details

--- python/course-notes/tools.py
+++ python/course-notes/tools.py
@@ -1,8 +1,9 @@
+# Aquivo criado para se utilizar junto com o arquivo "modulos-e-pip.py"
+
 PI = 3.14
 GRAVITY = 9.8
 
 # toda variável em maiúsculo é como se fosse uma "constante". Na prática não é 100% constante, mas é como se fosse
-
 
 
 def get_extension(file):
