18ff62e4f897e8dfdb45be4a65a2acc0144c0150
João Gabriel Savioli <joaosavioli06@gmail.com>
2023-07-20 15:44:25 -0300

    Creating a  folder and study files

---
+++ python/course-notes/orientacao-a-objetos/utilizar-class.py
@@ -0,0 +1,32 @@
+from carro import Carro 
+
+fusca = Carro("Volks", "Fusca", "Azul", "Gasolina")
+
+fusca.ligar()
+fusca.ligar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.frear()
+fusca.frear()
+fusca.frear()
+fusca.frear()
+fusca.frear()
+fusca.frear()
+fusca.desligar()
+fusca.desligar()
+
+onix = Carro("Chevrolet", "Onix", "Branco", "Etanol")
+celta = Carro("Chevrolet", "Celta", "Preto", "Gasolina")
+
+# Temos 3 OBJETOS diferentes, os três estão utilizando a CLASSE Carro. A diferença é que eles tem acesso às mesmas funções, porém eles tem atributos diferentes.
+
+onix.ligar()
+celta.ligar()
+
+onix.desligar()
+celta.desligar()
+
