ec5c1613a11c4519a9373338ca9cc27aabe88340
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-11-28 22:30:24 -0300

    Study file involving ! condition

---
+++ JavaScript/nomes.js
@@ -0,0 +1,18 @@
+let Nome1 = true
+let Nome2 = true
+let Nome3 = true
+
+let neg1 = !Nome1
+let neg2 = !Nome2
+let neg3 = true
+
+if (neg1 == true) {
+    console.log('A pessoa 1 é a verdadeira')
+} else if (neg2 == true) {
+    console.log('A pessoa 2 é a verdadeira')
+} else if (neg3 == true) {
+    console.log('A pessoa 3 é a verdadeira')
+} else {
+    console.log('Não existe nenhuma pessoa verdadeira aqui :(')
+}
+
