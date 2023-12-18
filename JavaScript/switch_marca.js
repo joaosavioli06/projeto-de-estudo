cd6f5f6be6b48657bffd7f84144d5e129d205484
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-12-18 14:47:41 -0300

    Adding switch study files

---
+++ JavaScript/switch_marca.js
@@ -0,0 +1,15 @@
+let marca = 'New Era'
+
+switch(marca){
+    case 'Nike':
+        console.log('A marca da sua camiseta é Nike')
+        break
+    case 'Lacoste':
+        console.log('A marca da sua camiseta é Lacoste')
+        break
+    case 'New Era':
+        console.log('A marca da sua camiseta é New Era')
+        break
+    default:
+        console.log('A marca que você escolheu não existe')
+}
---
+++ JavaScript/switch_number.js
@@ -0,0 +1,36 @@
+let number = 0
+
+switch (number){
+    case 1: 
+    console.log('O número escolhido é: 1')
+    break
+    case 2: 
+    console.log('O número escolhido é: 2')
+    break
+    case 3: 
+    console.log('O número escolhido é: 3')
+    break
+    case 4: 
+    console.log('O número escolhido é: 4')
+    break
+    case 5: 
+    console.log('O número escolhido é: 5')
+    break
+    case 6: 
+    console.log('O número escolhido é: 6')
+    break
+    case 7: 
+    console.log('O número escolhido é: 7')
+    break
+    case 8: 
+    console.log('O número escolhido é: 8')
+    break
+    case 9: 
+    console.log('O número escolhido é: 9')
+    break
+    case 10: 
+    console.log('O número escolhido é: 10')
+    break
+    default:
+        console.log('O número escolhido não existe no sistema')
+}
