3b6625c032011608e25f6bd19740069bbe6e24d9
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-12-18 13:39:07 -0300

    Adding study file about operator precedence

---
+++ JavaScript/precedencia_operadores.js
@@ -0,0 +1,17 @@
+let conta1 = 5 + 6 * 2
+console.log(conta1)
+
+let conta2 = (5 + 6) * 2
+console.log(conta2)
+
+let conta3 = 50 + 40 + 1 * 2 * 3
+console.log(conta3)
+
+let conta4 = (50 + 80) + 10 * 2 * 3
+console.log(conta4)
+
+// Precedência dos operadores: operações que vem primeiro 
+// 1º Parêntesis;
+// 2º Expoentes;
+// 3º Multiplicações e Divisões; 
+// 4º Somas e Subtrações. 
