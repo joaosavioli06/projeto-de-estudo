80a8b8e756da5a102e583da666b67388e50864c7
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-12-18 15:08:00 -0300

    Adding loop's study file

---
+++ JavaScript/loop_pratica.js
@@ -0,0 +1,11 @@
+for (i = 10; i <= 20; i++){
+    console.log('Número ' + i)
+}
+
+for (i = 1; i <= 10; i++){
+    console.log('Meu nome é João Gabriel Savioli')
+}
+
+for (i = 1; i <= 100; i++){
+    console.log('Número ' + i)
+}
