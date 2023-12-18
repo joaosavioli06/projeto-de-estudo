ecbcf74084ba9f556b80c59273a8542cbfa39c17
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-12-18 13:40:59 -0300

    Delete JavaScript/verificacao_or.js

--- JavaScript/verificacao_or.js
+++
@@ -1,9 +0,0 @@
-let corCliente = 'gray'
-let corSistema = 'black'
-
-let corVendida = corCliente || corSistema
-console.log(corVendida)
-
-// utilizando o operador ||, se o primeiro valor foi definido, ele vai retornar o primeiro valor, porque ou é um ou outro
-// ele sempre verificar da esquerda para direita
-
