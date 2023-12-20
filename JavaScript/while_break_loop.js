0be9e11a278370af022bf4d0a99efee217f3584d
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-12-20 14:16:36 -0300

    Adding "break loop" study file

---
+++ JavaScript/while_break_loop.js
@@ -0,0 +1,15 @@
+let j = 1
+
+while (j <= 15){
+    if (j === 12) break
+    console.log(j)
+    j++
+}
+
+let k = 2
+
+while (k <= 20){
+    if (k === 17) break
+    console.log('\nNúmero:\n', k)
+    k++
+}
