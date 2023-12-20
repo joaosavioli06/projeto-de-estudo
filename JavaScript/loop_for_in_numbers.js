e317a6deb1445b7dfffb5fe9eb263b20fb0b3d57
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-12-20 11:58:46 -0300

    Adding "loop for in" study files

---
+++ JavaScript/loop_for_in_numbers.js
@@ -0,0 +1,15 @@
+const numbers = {
+    number1: 1,
+    number2: 10,
+    number3: 50,
+    number4: 100,
+    number5: 500,
+    number6: 670,
+    number7: 745,
+    number8: 1000,
+    number9: 1250,
+    number10: 1720,
+}
+
+for (i in numbers)
+console.log(i, numbers[i])
---
+++ JavaScript/loop_for_in_tshirt.js
@@ -0,0 +1,15 @@
+const Tshirt = {
+    size: 'M',
+    color: 'Red',
+    brand: 'Adidas',
+    fabrication_country: 'USA',
+    price: 'R$ 159,90',
+    shop: 'Catarina Outlet',
+    city_address: 'São Roque',
+    shirt_id: 14
+}
+
+for (j in Tshirt){
+    console.log(j, Tshirt[j])
+}
+
