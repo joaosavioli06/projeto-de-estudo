b9a120566e612ee6bc8ce79e07739df9989d55cd
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-12-20 15:51:05 -0300

    Adding a random study file

---
+++ JavaScript/numero_entre.js
@@ -0,0 +1,9 @@
+let num = 10
+
+if (num >= 0 && num <= 3) 
+    console.log('Seu número está entre 0 e 3')
+ else if (num >= 4 && num <= 8)
+    console.log('Seu número está entre 4 e 8')
+else if (num >= 9 && num <= 12)
+    console.log('Seu número está entre 9 e 12')
+else console.log('Seu número é maior do que 12')
