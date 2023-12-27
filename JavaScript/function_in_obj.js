6cfb792ee8f5e6d276f762a65c88be8223b33036
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2023-12-27 14:12:23 -0300

    Adding OOP study files
    Files involving functions inside objects

---
+++ JavaScript/function_in_obj.js
@@ -0,0 +1,11 @@
+facens = {
+    endereco: 'Jd Iguatemi',
+    tamanho: 'Grande',
+    local: 'Faculdade'
+
+    ,distancia: function(){
+        console.log('Indo para a Facens! A distância não é pequena')
+    }
+}
+
+facens.distancia()
---
+++ JavaScript/function_in_obj2.js
@@ -0,0 +1,17 @@
+senac = {
+    endereco: 'Nogueira Padilha',
+    entrada: 'A pé, carro ou moto',
+    precoEm: 800,
+    avaliacao: 2
+
+    ,ingles: function(){
+        console.log('Tirei nota máxima no teste de Inglês!')
+    }
+
+    ,excel: function(){
+        console.log('Realizei o curso de Excel com o Trovão!')
+    }
+}
+
+senac.ingles()
+senac.excel()
