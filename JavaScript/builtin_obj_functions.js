10f36dc7b1c2debf55b0985e900e020c5f65f8af
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-01-05 14:59:11 -0300

    JS study file about Built-in objects

---
+++ JavaScript/builtin_obj_functions.js
@@ -0,0 +1,14 @@
+function PImult (num){
+    PI = Math.PI
+    console.log(PI * num)
+    return
+}
+
+PImult(10)
+
+function Roundnum (num){
+    console.log(Math.round(num))
+    return
+}
+
+Roundnum(15.5)
