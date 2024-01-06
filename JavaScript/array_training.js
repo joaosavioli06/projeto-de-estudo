0d0439dd1a9edc88cdb2d20c17115b896ea6bc82
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-01-06 15:13:40 -0300

    Adding JS study file about Arrays
    Array training

---
+++ JavaScript/array_training.js
@@ -0,0 +1,49 @@
+let contagem = [9, 10]
+
+contagem.push(11, 12)
+
+contagem.unshift(1, 2, 3, 4, 5)
+
+contagem.splice(5, 0, 6, 7, 8)
+
+console.log(contagem)
+
+// Feito!
+
+let num = [12, 13, 14]
+
+num.push(15, 16, 17, 18, 19, 20)
+
+num.unshift(1, 2, 3, 4, 5)
+
+num.splice(5, 0 , 6, 7, 8, 9, 10, 11)
+
+console.log(num)
+
+// Feito!
+
+let num2 = [15]
+
+num2.push(18, 19, 20)
+
+num2.unshift(1,2,3,4)
+
+num2.splice(4, 0, 5, 6, 7, 8, 9)
+
+num2.splice(9, 0, 10, 11, 12, 13, 14)
+
+num2.splice(15, 0, 16, 17 )
+
+console.log(num2)
+
+// Feito!
+
+let num3 = [8]
+
+num3.unshift(1,2 ,3)
+
+num3.splice(3, 0 , 4, 5, 6, 7)
+
+num3.push(9, 10)
+
+console.log(num3)
