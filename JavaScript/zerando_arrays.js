0cb17d0d99249d5956a43744d4aa0cfe05eb1199
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-01-07 16:58:30 -0300

    JS study file involving emptying arrays

---
+++ JavaScript/zerando_arrays.js
@@ -0,0 +1,37 @@
+let lista = ['João', 'Fernanda', 'Ester', 'Daniel', 'Isaque', 'Raquel']
+
+lista = []
+
+lista.splice(0, lista.length)
+
+lista.length = 0
+
+console.log(lista)
+
+let listaFutura = ['John', 'Nanda', 'Dog', 'Flat', 'Car']
+
+listaFutura.splice(1, 3)
+
+listaFutura.pop()
+
+listaFutura.shift()
+
+console.log(listaFutura)
+
+list = [1,2,3,4,5,6,7,8,9,10]
+
+list = []
+
+console.log(list)
+
+list2 = [1,2]
+
+list2.length = 0
+
+console.log(list2)
+
+list3 = [1,23,34,4,44,]
+
+list3.splice(0, list3.length = 0)
+
+console.log(list3)
