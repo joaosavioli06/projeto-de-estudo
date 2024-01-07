b98989689692e8d004529b4ec548cc9707531e5c
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-01-07 17:01:39 -0300

    JS study file about removing items in an array

---
+++ JavaScript/removing_items_array.js
@@ -0,0 +1,11 @@
+const lista = ['Frank', 'Sidoka', 'Manchester', 'Flea', 'Houston']
+
+const ult = lista.pop()
+
+const fir = lista.shift()
+
+const vaz = lista.splice(0, 3)
+
+console.log(lista)
+
+console.log(vaz)
