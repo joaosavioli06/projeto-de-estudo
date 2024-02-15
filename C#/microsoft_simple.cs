d98867918cb5a5a71838ba2245588dd1f909a08e
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-02-15 15:30:47 -0300

    Basic algorithm made on Microsoft Learn
    Desafio: Exibir valores literais e variáveis
    
    Unidade 6 de 9

---
+++ C#/microsoft_simple.cs
@@ -0,0 +1,7 @@
+string name = "Bob";
+int numberMessages = 3;
+float temperature = 34.4f;
+
+Console.Write("Hello, " + name + "! ");
+Console.Write("You have " + numberMessages + " messages in your inbox. ");
+Console.Write("The temperature is " + temperature + " celsius.");
