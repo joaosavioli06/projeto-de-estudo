f7c97ea8d4da9abf86d39dca5156a1e0b4861002
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-02-19 18:45:10 -0300

    Adding C# learning files

---
+++ C#/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Olá! Meu nome é João Gabriel!");
+
+Console.Write("\nÉ assim");
+Console.Write(" que pula ");
+Console.Write("às linhas\n\n");
+
+
+char firstLetter = 'J';
+Console.Write(firstLetter);
+
+char secondLetter = 'G';
+Console.Write(secondLetter);
+
+char thirdLetter = 'S';
+Console.Write(thirdLetter);
+
+Console.WriteLine("\n\nMeu nome (João Gabriel) começar com a letra " +  firstLetter);
---
+++ C#/calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Calculator()
+
+{
+    static void Main()
+    {
+
+    int num1 = 10;
+    int num2 = 20;
+
+    int total = num1 + num2;
+
+    Console.WriteLine(total);
+
+    }
+}
