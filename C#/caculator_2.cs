94f5cd069e070d4640ba8ab599c0e675327995e1
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-02-22 14:55:22 -0300

    Addinc C# study file

---
+++ C#/caculator_2.cs
@@ -0,0 +1,19 @@
+﻿namespace study_project
+{
+    internal class caculator_2
+    {
+        static void Main()
+        {
+            int num1;
+            int num2;
+            int num3;
+
+            num1 = 5;
+            num2 = 10;
+            num3 = 15;
+
+            int total = num1 + (num2 / num3);
+            Console.WriteLine(total);
+        }
+    }
+}
