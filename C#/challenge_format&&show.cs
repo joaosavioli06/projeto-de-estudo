bfdd76e7aa91f612f203fae8cc19466828f53010
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-02-22 17:45:27 -0300

    Fixing an error
    New line:
    Console.Write($"View English output:\nc:\\Exercise\\{projectName}\\data.txt\n\n");

--- C#/challenge_format&&show.cs
+++ C#/challenge_format&&show.cs
@@ -4,7 +4,7 @@ string projectName = "ACME";
 /* RU */
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
-Console.Write($"View English output:" + "\n" + "c:\\Exercise\\{projectName}\\data.txt\n\n");
+Console.Write($"View English output:\nc:\\Exercise\\{projectName}\\data.txt\n\n");
 
 Console.WriteLine($@"{russianMessage}:
 c:\Exercise\{projectName}\ru-RU\data.txt");
